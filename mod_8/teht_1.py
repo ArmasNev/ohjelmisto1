63fe5e0be673d515d153bad3251ad84908d8c234
ArmasNev <armas.n82@gmail.com>
2023-09-13 12:39:36 +0300

    Moduuli_8

---
+++ mod_8/teht_1.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='6661507',
+         autocommit=True
+         )
+def getAirportByICAO(ICAO):
+    sql = "SELECT name, municipality FROM airport"
+    sql += " WHERE ident='" + ICAO + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount >0 :
+        for row in result:
+            print(f" This is {row[0]} at {row[1]} ")
+    else:
+        print("Virheellinen ICAO-koodi. Lentokenta ei löytynyt.")
+    return
+ICAO = input("Laita lentokentan ICAO-koodi: ").upper()
+getAirportByICAO(ICAO)

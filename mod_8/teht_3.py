37141d3d00bec2dc5d81967150114c23d7ba3d68
ArmasNev <armas.n82@gmail.com>
2023-09-13 16:07:02 +0300

    Moduuli_8

---
+++ mod_8/teht_3.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from geopy import distance
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='6661507',
+         autocommit=True
+         )
+def getAirportByICAO(ICAO):
+    sql = "SELECT latitude_deg, longitude_deg FROM airport"
+    sql += " WHERE ident='" + ICAO + "'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount >0 :
+        return result
+    else:
+        print("Virheellinen ICAO-koodi. Lentokenta ei löytynyt.")
+    return
+ICAO = input("Laita lentokentan ICAO-koodi: ").upper()
+Lentokennta1 = getAirportByICAO(ICAO)
+ICAO = input("Laita lentokentan ICAO-koodi: ").upper()
+Lentokennta2 = getAirportByICAO(ICAO)
+print(f"Lentokenttien välisen etäisyys on: {int(distance.distance(Lentokennta1, Lentokennta2).km)} km.")
